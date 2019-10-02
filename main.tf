d963380691d21d37ed9706af674992f9dc8a4da8
Derek Anderson <derek.anderson@thinkahead.com>
2019-10-02 10:51:15 -0400

    Added workspace output

--- main.tf
+++ main.tf
@@ -1,13 +1,17 @@
-resource "random_id" "server" {
-  byte_length = 8
+resource "random_id" "hash" {
+  byte_length = "${var.byte_length}"
 }
 
 output "hash" {
-  value       = "${random_id.server.hex}"
-  description = "Generated hash"
+  value       = "${random_id.hash.hex}"
+  description = "hash hash"
 }
 
 variable "byte_length" {
   description = "How long to make the generated hash"
   default     = 8
+}
+
+output "workspace" {
+  value = terraform.workspace
 }
